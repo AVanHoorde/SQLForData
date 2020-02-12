cfecf9d26ef02aec8b7406b4358b500cf5ecbb58
Louis <louis-laugier@hotmail.fr>
2020-02-12 15:13:34 +0100

    addDB

--- app.py
+++ app.py
@@ -2,18 +2,38 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import psycopg2
 
+# database
+def dbconnect():
+    try:
+        connection = psycopg2.connect(host="ec2-46-137-177-160.eu-west-1.compute.amazonaws.com",database="dbntjps2ogiaoe", user="jqwpptjnjyvaqw", password="cb6d58e6c0a9e078767b32777fe990054d317a029116a89fd35a8a29e974dcfc")
+        # create a cursor
+        cur = connection.cursor()
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
+
+
+# front-end
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
-
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
-
     dcc.Graph(
         id='example-graph',
         figure={
@@ -27,6 +47,6 @@ app.layout = html.Div(children=[
         }
     )
 ])
-
 if __name__ == '__main__':
+    dbconnect()
     app.run_server(debug=True)
